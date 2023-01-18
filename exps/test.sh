ea6b1be2ab84a9a621f3a9311ea0cc5828ece23d
students <aiarhipov@lorien.atp-fivt.org>
2023-01-18 13:37:38 +0000

    add pkl to .gitignore and test.sh

---
+++ exps/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+conda activate mmdet_edit
+python /home/aiarhipov/mmdetection/tools/test.py \
+    /home/aiarhipov/centernet/exps/16_paper_params_dla34_batch8/config.py \
+    /home/aiarhipov/centernet/exps/16_paper_params_dla34_batch8/epoch_150.pth \
+    --out /home/aiarhipov/centernet/exps/16_paper_params_dla34_batch8/res_output.pkl \
+    --eval bbox
+python /home/aiarhipov/mmdetection/tools/analysis_tools/analyze_results.py \
+    /home/aiarhipov/centernet/exps/16_paper_params_dla34_batch8/config.py \
+    /home/aiarhipov/centernet/exps/16_paper_params_dla34_batch8/res_output.pkl \
+    /home/aiarhipov/centernet/exps/16_paper_params_dla34_batch8/res_samples \ 
+    --topk 50 \
+    --show-score-thr 0.3
