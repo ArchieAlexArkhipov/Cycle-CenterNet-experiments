c6b92d340e94fb7873f312cbc88b1114644bfccc
students <aiarhipov@lorien.atp-fivt.org>
2023-01-18 13:50:44 +0000

    upd test.sh and metrics for 15, 16

--- exps/test.sh
+++ exps/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 conda activate mmdet_edit
 python /home/aiarhipov/mmdetection/tools/test.py \
     /home/aiarhipov/centernet/exps/16_paper_params_dla34_batch8/config.py \
@@ -6,8 +7,8 @@ python /home/aiarhipov/mmdetection/tools/test.py \
     --out /home/aiarhipov/centernet/exps/16_paper_params_dla34_batch8/res_output.pkl \
     --eval bbox
 python /home/aiarhipov/mmdetection/tools/analysis_tools/analyze_results.py \
-    /home/aiarhipov/centernet/exps/16_paper_params_dla34_batch8/config.py \
-    /home/aiarhipov/centernet/exps/16_paper_params_dla34_batch8/res_output.pkl \
-    /home/aiarhipov/centernet/exps/16_paper_params_dla34_batch8/res_samples \ 
+    /home/aiarhipov/centernet/exps/15_paper_params_resnet34_batch8/config.py \
+    /home/aiarhipov/centernet/exps/15_paper_params_resnet34_batch8/res_output.pkl \
+    /home/aiarhipov/centernet/exps/15_paper_params_resnet34_batch8/ \
     --topk 50 \
     --show-score-thr 0.3
